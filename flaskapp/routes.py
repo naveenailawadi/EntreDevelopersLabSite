d57afd23fb7598dd245ead1bf28ff7c7ac6d88b7
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2021-02-02 21:58:36 +0000

    added a comma

--- flaskapp/routes.py
+++ flaskapp/routes.py
@@ -40,7 +40,7 @@ def apply():
 
 @app.route("/virgins")
 def virgins():
-    choices = ['https://www.linkedin.com/in/changhao-leo-wang-50b5b819a/', 'https://www.linkedin.com/in/anthony-petruzziello-8b3196167/'
+    choices = ['https://www.linkedin.com/in/changhao-leo-wang-50b5b819a/', 'https://www.linkedin.com/in/anthony-petruzziello-8b3196167/',
                'https://www.linkedin.com/in/andrew-caminiti-339a2518b/', 'https://www.linkedin.com/in/perry-guloien-b4746b14b/',
                'https://www.linkedin.com/in/anika-prakash-000410168/', 'https://www.linkedin.com/in/aryaman-k-019379135/']
 
